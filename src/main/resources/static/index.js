e60eafb38324c2b792b17df2bbb3a7a00a90c7ff
Sattu Charan <sushanthmr208@gmail.com>
2026-07-18 11:58:52 +0530

    MRF

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,1 +1,32 @@
-alert("open on your own risk")
+var users = [
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
